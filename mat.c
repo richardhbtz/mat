31491f36195021247bb114bf2f830554b0142c48
richardhbtz <richardhabitzreuter@protonmail.com>
2023-12-12 02:10:48 -0300

    getWindowSize

--- mat.c
+++ mat.c
@@ -1,38 +1,49 @@
 // includes
-#include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+#include <sys/ioctl.h>
+
 // defines
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 // data
-struct termios orig_termios;
+struct editorConfig
+{
+	int screenRws, screencls;
+	
+	struct termios orig_termios;
+};
+
+struct editorConfig E;
 
 // terminal
 void die(const char *s)
 {
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
 	perror(s);
 	exit(1);
 }
 
 void disableRawMode()
 {
-	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
 		die("tcsetattr");
 }
 
 void enableRawMode()
 {
-	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+	if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1)
 		die("tcgetattr");
 
 	atexit(disableRawMode);
 
-	struct termios raw = orig_termios;
+	struct termios raw = E.orig_termios;
 
 	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
 	raw.c_oflag &= ~(OPOST);
@@ -46,7 +57,43 @@ void enableRawMode()
 		die("tcsetattr");
 }
 
-char ReadKey()
+int getWindowSize(int *rws, int *cls)
+{
+	struct winsize ws;
+	if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+	{
+		return -1;
+	}
+	else
+	{
+		*rws = ws.ws_row;
+		*rws = ws.ws_col;
+		return 0;
+	}
+}
+
+// output
+void drawRows()
+{
+	int y;
+	for (y = 0; y < E.screenRws; y++)
+	{
+		write(STDOUT_FILENO, "~\r\n", 3);
+	}
+}
+
+void refreshScreen()
+{
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
+	drawRows();
+
+	write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+// input
+char readKey()
 {
 	int nread;
 	char c;
@@ -58,26 +105,38 @@ char ReadKey()
 	return c;
 }
 
-void HandleKeyPress()
+void handleKeyPress()
 {
-	char c = ReadKey();
+	char c = readKey();
 
 	switch (c)
 	{
 	case CTRL_KEY('q'):
+		write(STDOUT_FILENO, "\x1b[2J", 4);
+		write(STDOUT_FILENO, "\x1b[H", 3);
 		exit(0);
 		break;
 	}
 }
 
 // init
+void init()
+{
+	if (getWindowSize(&E.screenRws, &E.screencls) == -1)
+	{
+		die("getWindowSize");
+	}
+}
+
 int main(int argc, char *argv[])
 {
 	enableRawMode();
+	init();
 
 	while (1)
 	{
-		HandleKeyPress();
+		refreshScreen();
+		handleKeyPress();
 	}
 
 	disableRawMode();
